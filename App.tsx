377fd5e2b5b0775d570cba8b29e042738056f042
Brad Stapleton <brad@grifin.com>
2025-09-18 00:21:45 -0400

    Created a simple calculator

--- App.tsx
+++ App.tsx
@@ -5,16 +5,46 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+const multiplier = (n1: number, n2: number) => n1 * n2;
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [num1, setNum1] = useState<number>(0);
+  const [num2, setNum2] = useState<number>(0);
+
+  const createTextChangedEvent = (numberSetter: (newNumber: number) => void) => {
+    const onTextChanged = (updatedText: string) => {
+      const sanitizedInput = updatedText.replaceAll(/[^0-9]/g, '');
+      numberSetter(Number(sanitizedInput));
+    };
+
+    return onTextChanged;
+  }
+  
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text>First Number</Text>
+      <TextInput
+        style={styles.input}
+        value={String(num1)}
+        onChangeText={createTextChangedEvent(setNum1)}
+      />
+      <Text>Second Number</Text>
+      <TextInput
+        style={styles.input}
+        value={String(num2)}
+        onChangeText={createTextChangedEvent(setNum2)}
+      />
+      <Text>Product</Text>
+      <Text>{multiplier(num1, num2)}</Text>
     </View>
   );
 }
@@ -22,7 +52,12 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 30,
   },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+  }
 });
 
 export default App;
